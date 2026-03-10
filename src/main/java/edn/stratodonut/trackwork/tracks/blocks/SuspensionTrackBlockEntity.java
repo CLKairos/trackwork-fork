b6568412030303e79ccb26d1d91b6878c32f1aa9
endalion <34496536+Endalion@users.noreply.github.com>
2026-03-10 21:17:05 +0800

    Fix damage hit testing with ship rotation

--- src/main/java/edn/stratodonut/trackwork/tracks/blocks/SuspensionTrackBlockEntity.java
+++ src/main/java/edn/stratodonut/trackwork/tracks/blocks/SuspensionTrackBlockEntity.java
@@ -31,6 +31,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.joml.Intersectiond;
 import org.joml.Math;
 import org.joml.Vector3d;
 import org.joml.Vector3dc;
@@ -39,6 +40,7 @@ import org.valkyrienskies.core.api.ships.ServerShip;
 import org.valkyrienskies.core.api.ships.Ship;
 import org.valkyrienskies.core.impl.bodies.properties.BodyKinematicsImpl;
 import org.valkyrienskies.mod.common.VSGameUtilsKt;
+import org.valkyrienskies.mod.common.util.IEntityDraggingInformationProvider;
 
 import java.util.List;
 import java.util.Random;
@@ -227,14 +229,40 @@ public class SuspensionTrackBlockEntity extends TrackBaseBlockEntity implements 
                 this.wheelTravel = newWheelTravel;
 
                 // Entity Damage
-                List<LivingEntity> hits = this.level.getEntitiesOfClass(LivingEntity.class, new AABB(this.getBlockPos())
+                AABB trackAabb = new AABB(this.getBlockPos())
                         .deflate(0.25)
-                        .expandTowards(0, -1.5, 0)
-                );
-                Vec3 worldPos = toMinecraft(ship.getShipToWorld().transformPosition(toJOML(Vec3.atCenterOf(this.getBlockPos()))));
+                        .expandTowards(0, -1.5, 0);
+                List<LivingEntity> hits = this.level.getEntitiesOfClass(LivingEntity.class, trackAabb);
+                Vec3 worldPos = toMinecraft(
+                        ship.getShipToWorld().transformPosition(toJOML(Vec3.atCenterOf(this.getBlockPos()))));
+
+                Vector3d b0c = ship.getShipToWorld().transformPosition(toJOML(trackAabb.getCenter()));
+                Vector3d b0hs = new Vector3d(trackAabb.getXsize() / 2.0, trackAabb.getYsize() / 2.0,
+                        trackAabb.getZsize() / 2.0);
+                Vector3d b0uX = ship.getTransform().getShipToWorldRotation().transform(new Vector3d(1, 0, 0));
+                Vector3d b0uY = ship.getTransform().getShipToWorldRotation().transform(new Vector3d(0, 1, 0));
+                Vector3d b0uZ = ship.getTransform().getShipToWorldRotation().transform(new Vector3d(0, 0, 1));
+
                 for (LivingEntity e : hits) {
+                    Ship mountedShip = VSGameUtilsKt.getShipMountedTo(e);
+                    if (mountedShip != null && mountedShip.getId() == ship.getId()) continue;
+                    Vector3d b1c = toJOML(e.getBoundingBox().getCenter());
+                    Vector3d b1hs = new Vector3d(e.getBbWidth() / 2.0, e.getBbHeight() / 2.0, e.getBbWidth() / 2.0);
+                    Vector3d b1uX = new Vector3d(1, 0, 0);
+                    Vector3d b1uY = new Vector3d(0, 1, 0);
+                    Vector3d b1uZ = new Vector3d(0, 0, 1);
+
+                    if (mountedShip != null) {
+                        b1uX = mountedShip.getTransform().getShipToWorldRotation().transform(b1uX);
+                        b1uY = mountedShip.getTransform().getShipToWorldRotation().transform(b1uY);
+                        b1uZ = mountedShip.getTransform().getShipToWorldRotation().transform(b1uZ);
+                    }
+
+                    if (!Intersectiond.testObOb(b0c, b0uX, b0uY, b0uZ, b0hs, b1c, b1uX, b1uY, b1uZ, b1hs)) {
+                        continue;
+                    }
+
                     push(e, worldPos);
-                    Vec3 relPos = e.position().subtract(worldPos);
                     float speed = Math.abs(this.getSpeed());
                     if (speed > 1) e.hurt(TrackDamageSources.runOver(this.level), (speed / 8f) * AllConfigs.server().kinetics.crushingDamage.get());
                     if (e instanceof ServerPlayer p) p.connection.send(new ClientboundSetEntityMotionPacket(p));
